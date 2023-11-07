83badaf39baa3e0ad7e2d15669308fa7018f6dc2
mandeep <itsonlymandeep@gmail.com>
2023-11-07 22:16:12 +0530

    chart js added for all graphs

---
+++ pages/chart.js
@@ -0,0 +1,13 @@
+/* https://blog.logrocket.com/using-chart-js-react */
+
+import React from 'react'
+
+function chart() {
+  return (
+    <div>
+      Chart
+    </div>
+  )
+}
+
+export default chart
